764df327f9f734fd9e5f76f63c4fd3766b3eb182
Egorka-drug <sashamorozov907@gmail.com>
2026-05-11 20:57:13 +0300

    Update keyboard.py

--- app/keyboard.py
+++ app/keyboard.py
@@ -17,9 +17,9 @@ start_mes = ReplyKeyboardMarkup(keyboard=[
 )
 
 menu_mes = ReplyKeyboardMarkup(keyboard=[
-  [KeyboardButton(text = '💼 Dos'),KeyboardButton(text='👁️ Глаз Бога')],
+  [KeyboardButton(text = '💣 Бомбер'),KeyboardButton(text='👁️ Глаз Бога')],
   [KeyboardButton(text='📊 Статистика'),KeyboardButton(text='💰 Пополнить')],
-  [KeyboardButton(text='🕵️ Мой профиль'),KeyboardButton(text='🔵 VK')]
+  [KeyboardButton(text='🕵️ ️Мой профиль'),KeyboardButton(text='🔵 VK')]
 
 ],
   resize_keyboard=True
